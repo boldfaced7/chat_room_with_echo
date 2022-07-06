612af8b891b2793f24a86e8e246e7ef8cad23948
준모 강 <boldfaced7@hanyang.ac.kr>
2022-07-06 19:45:32 +0900

    feat: 템플릿을 이용해, 로직과 뷰 분리

--- chat/main.go
+++ chat/main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"github.com/labstack/echo"
+	"html/template"
+	"io"
 	"net/http"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("./chat/templates/*.html")),
+	}
 	e := echo.New()
+	e.Renderer = t
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, `
-		<html>
-			<head>
-				<title>Chat</title>
-			</head>
-			<body>
-				Let's chat!
-			</body>
-		</html>
-		`)
+		return c.Render(http.StatusOK, "chat.html", "")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
