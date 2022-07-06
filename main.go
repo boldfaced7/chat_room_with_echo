12372d2d24a4015047be1c1edc53bd1ccbeab797
준모 강 <boldfaced7@hanyang.ac.kr>
2022-07-06 17:23:33 +0900

    feat: 간단한 웹 서버 생성

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
 func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, `
+		<html>
+			<head>
+				<title>Chat</title>
+			</head>
+			<body>
+				Let's chat!
+			</body>
+		</html>
+		`)
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
